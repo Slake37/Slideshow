d4204ac1d07937b0d5c5bd57da2c79a2d2b79fb2
Slake37 <moldovan.alin160@yahoo.co.uk>
2021-09-08 13:57:18 +0100

    new

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-let nextBtn = document.querySelector('#next')
-let leftSlide = document.querySelector('.left-side');
-let rightSlide = document.querySelector('.right-side');
+let leftImg = document.querySelectorAll('.left-img');
+let rightImg = document.querySelectorAll('.right-img');
+let nextImg = document.querySelector('.next');
+let prevImg= document.querySelector('.previous');
 
-
-
-    nextBtn.addEventListener('click', function(){
-    for(i=0; i<=100; i+25){
-        if (i=25)
-        leftSlide.style.transform = 'translateY('+i+'%)';
-        rightSlide.style.transform = 'translateY(-'+i+'%)';
-        break;
-    }
+localStorage.setItem('value1', 0);
+nextImg.addEventListener('click',function(){
+    let i=localStorage.getItem('value1');
+    leftImg[i].style.top = '0';
+    rightImg[i].style.bottom = '0';
+    i++;
+    localStorage.setItem('value1', `${i}`);
+    console.log(i);
+    
 })
+
+
+
+prevImg.addEventListener('click',function(){
+   let j=localStorage.getItem('value1')-1;
+   leftImg[j].style.top = '100%'
+   rightImg[j].style.bottom = '100%'
+    console.log(j);
+    localStorage.setItem('value1', `${j}`);
+    j--;
+   
+    }
+)
+
+
+
+
+
+
 
